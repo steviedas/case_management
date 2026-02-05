5deb3629cb1c286cbcfecb1db01a918cecfe8656
Steven Das <steviedas@hotmail.com>
2026-02-05 10:54:37 +0000

    Reordered drop table statements

--- stage_2/drop_star_schema.sql
+++ stage_2/drop_star_schema.sql
@@ -10,6 +10,18 @@ IF OBJECT_ID('dbo.bridge_case_intervention', 'U') IS NOT NULL
 IF OBJECT_ID('dbo.fact_record', 'U') IS NOT NULL
     DROP TABLE dbo.fact_record;
 
+IF OBJECT_ID('dbo.fact_case', 'U') IS NOT NULL
+    DROP TABLE dbo.fact_case;
+
+IF OBJECT_ID('dbo.dim_report_snapshot_row', 'U') IS NOT NULL
+    DROP TABLE dbo.dim_report_snapshot_row;
+
+IF OBJECT_ID('dbo.fact_report_snapshot', 'U') IS NOT NULL
+    DROP TABLE dbo.fact_report_snapshot;
+
+IF OBJECT_ID('dbo.fact_report', 'U') IS NOT NULL
+    DROP TABLE dbo.fact_report;
+
 IF OBJECT_ID('dbo.fact_alert', 'U') IS NOT NULL
     DROP TABLE dbo.fact_alert;
 
